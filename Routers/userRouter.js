4a5e056d8d064d1670d17c72a5482f208f09abab
chandran6799 <chandranak06071999@gmail.comgit config --global user.email chandranak06071999@gmail.com>
2024-07-09 19:50:51 -0700

    FitnessTracker Backend

---
+++ Routers/userRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { deleteUser, updateUser } from "../Controllers/userContorllers.js";
+import { verifyToken } from "../Middleware/verifyToken.js";
+
+
+const router = express.Router();
+
+router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+
+export default router;
